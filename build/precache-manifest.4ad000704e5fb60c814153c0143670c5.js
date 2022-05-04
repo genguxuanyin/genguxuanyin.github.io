82ce6905370e192a26814d139d1ee359f99fb655
genguxuanyin <forever_ydy@163.com>
2022-05-04 10:26:37 +0800

    add build

---
+++ build/precache-manifest.4ad000704e5fb60c814153c0143670c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fb1f16863e61e774836debea2bf7e5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfc93154eeb8f9aba931",
+    "url": "/static/css/main.3bf64506.chunk.css"
+  },
+  {
+    "revision": "8c950676ec207ca71548",
+    "url": "/static/js/2.80f5c260.chunk.js"
+  },
+  {
+    "revision": "41e8aedee29e6e7a9a1bdd0a210ba283",
+    "url": "/static/js/2.80f5c260.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfc93154eeb8f9aba931",
+    "url": "/static/js/main.8a8131bc.chunk.js"
+  },
+  {
+    "revision": "2a3496e0349049e73fa1",
+    "url": "/static/js/runtime-main.9aef5679.js"
+  }
+]);
